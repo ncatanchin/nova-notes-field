a79f25692a04a6ab654ff506b4e4f06ccc979fc5
Nicholas Catanchin <nicholas@catanchin.id.au>
2021-03-21 03:30:22 +0000

    Update migrations

---
+++ migrations/2021_03_19_000000_create_notes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Catanchin\NovaNotesField\NotesFieldServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNovaNotesTable extends Migration
+{
+    public function up()
+    {
+        if (! Schema::hasTable(NotesFieldServiceProvider::getTableName())) {
+            Schema::create(NotesFieldServiceProvider::getTableName(), function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->uuid('uuid');
+                $table->morphs('commentable');
+                $table->text('comment');
+                $table->boolean('is_approved')->default(false);
+                $table->unsignedBigInteger('user_id')->nullable();
+
+                $table->index('user_id');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists(NotesFieldServiceProvider::getTableName());
+    }
+}
